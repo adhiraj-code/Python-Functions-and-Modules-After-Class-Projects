e5dedb6bfd22dab47821d4888bc455e77fe1b69c
adhiraj-code <alkarathore87@gmail.com>
2026-03-30 14:46:08 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,8 @@
+radius = int(input("Enter the radius of the circle: "))
+pi = 3.14
+
+def circumference(radius):
+    circumference = 2 * pi * radius
+    return circumference
+
+print("The circumference of the circle is:", circumference(radius))
